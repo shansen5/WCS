52353f6d1f4b4545abe32418976b4b590a718d21
shansen5 <steven.e.hansen@gmail.com>
2015-09-21 22:25:35 -0700

    Clean up, add eth0, response to override of wireless network.

--- usr/sbin/wcs_network.py
+++ usr/sbin/wcs_network.py
@@ -62,13 +62,44 @@ def register_ip( ip_addr ):
   except:
     print "Unexpected error: ", sys.exc_info()[0]
 
-def is_connected():
+def is_eth0() :
+  retval = False
+  search1 = 'eth0'
+  search2 = 'inet addr:'
+  s = commands.getoutput( '/sbin/ifconfig' ).split( '\n' )
+  for line in s:
+    if search1 in line:
+      nextline = s[s.index(line)+1].lstrip()
+      if search2 in nextline:
+        i1 = nextline.find( search2 )
+        ip_addr = nextline[i1 + len( search2 ):].split()[0]
+        print 'Ethernet connected'
+        retval = True
+        register_ip( ip_addr )
+        # Turn off dnsmasq and hostapd services, as they are not
+        # needed and will consume cpu.  Only needed when the unit
+        # is an AP.
+        os.system( 'service dnsmasq stop' )
+        os.system( 'service hostapd stop' ) 
+        break
+  return retval
+
+def is_wlan0() :
+  search1 = 'wlan0'
+  s = commands.getoutput( '/sbin/ifconfig' ).split( '\n' )
+  for line in s:
+    if search1 in line:
+      return True
+  return False
+
+def is_connected( do_ifdown = True ):
   retval = False
   search1 = 'wlan0'
   search1a = 'mon.wlan0'
   search2 = 'inet addr:'
-  # do 'ifdown wlan0; ifup wlan0' to start the wireless interface.
-  os.system( 'ifdown wlan0; ifup wlan0' )
+  if do_ifdown :
+    # do 'ifdown wlan0; ifup wlan0' to start the wireless interface.
+    os.system( 'ifdown wlan0; ifup wlan0' )
   s = commands.getoutput( '/sbin/ifconfig' ).split( '\n' )
   for line in s:
     if search1 in line:
@@ -96,22 +127,31 @@ def is_connected():
         # is an AP.
         os.system( 'service dnsmasq stop' )
         os.system( 'service hostapd stop' ) 
-        # do 'ifdown wlan0; ifup wlan0' to start the wireless interface.
-        os.system( 'ifdown wlan0; ifup wlan0' )
+        # Don't see why I would need to take the interface down and up again.
+        # # do 'ifdown wlan0; ifup wlan0' to start the wireless interface.
+        # os.system( 'ifdown wlan0; ifup wlan0' )
         break
   return retval
 
-# Program starts here.
+def do_main() :
+  # If there is an ethernet connection, report it and stop.
+  if is_eth0() :
+    return
 
-time.sleep(10)
-# pdb.set_trace()
+  # If there is no wlan0 connection, we can't go further.
+  if not is_wlan0() :
+    return
 
-# See if we're already connected and don't need to proceed further.
-connected = is_connected()
+  time.sleep(10)
 
-if connected == False:
-  # Try to connect with a configuration specified in a
-  # file on the USB stick, if it exists.
+  # Read the configuration specified in a file on the USB stick,
+  # if it exists.
+  # If it contains "override true" then use the configuration specified
+  # even if we could connect to the network we were previously on.
+  connected = False
+  ssid = ""
+  psk = ""
+  override = False
   if os.path.exists( config_file ):
     try:
       with open( config_file, 'r' ) as cfg:
@@ -122,8 +162,6 @@ if connected == False:
       print "I/O error({0}): {1}".format( errno, strerror )
     print read_data
     # read_data may contain a value for ssid and wpa-psk
-    ssid = ""
-    psk = ""
     for line in read_data:
       i1 = line.find( 'wpa-ssid' )
       if i1 != -1:
@@ -131,8 +169,20 @@ if connected == False:
       i1 = line.find( 'wpa-psk' )
       if i1 != -1:
         psk = line
+      if "override" in line and "true" in line:
+        override = True
+
+  # See if we're already connected and don't need to proceed further.
+  # If override is set, skip this so we'll always to to the new network
+  # that has been requested.
+  if override == False:
+    connected = is_connected()
+
+  if connected == False:
+    # Try to connect with the configuration specified from the
+    # file on the USB stick.
     if ssid != "":
-      # if so, open interfaces.wpa and replace these values in that file.
+      # if there is an ssid, open interfaces.wpa and replace these values in that file.
       # backup the current interfaces.wpa to one with a timestamp appended
       # to the file name.
       # if read_data has a wpa-ssid and no wpa-psk, delete any existing
@@ -163,25 +213,33 @@ if connected == False:
       # if everything works, get the IP address and report it to the
       # web service and set connected to True.
       # Test the new connection.
-      connected = is_connected()
+      connected = is_connected( False )
       # Give it another try if it failed, in case there was a timing issue.
       if connected == False:
         time.sleep(10)
         os.system( 'ifdown wlan0; ifup wlan0' )
-        connected = is_connected()
+        connected = is_connected( False )
 
-if connected == False:
-  # No network available.  Configure to be a wireless access point (AP).
-  # Copy /etc/network/interfaces.hostapd to /etc/network/interfaces,
-  # after backing up interfaces to have a timestamp appended to its
-  # file name.
-  # do 'ifdown wlan0; ifup wlan0' to start the wireless interface.
-  # do 'service dnsmasq restart' and 'service hostapd restart'.
-  # This may not work, so the ifconfig may show an address but the
-  # antenna isn't broadcasting.
-  backup( '/etc/network/', 'interfaces' )
-  os.system( 'cp ' + interfaces_ap_file + ' /etc/network/interfaces' )
-  time.sleep(10)
-  os.system( 'ifdown wlan0; ifup wlan0' )
-  os.system( 'service dnsmasq restart' )
-  os.system( 'service hostapd restart' ) 
+  if connected == False:
+    # No network available.  Configure to be a wireless access point (AP).
+    # Copy /etc/network/interfaces.hostapd to /etc/network/interfaces,
+    # after backing up interfaces to have a timestamp appended to its
+    # file name.
+    # do 'ifdown wlan0; ifup wlan0' to start the wireless interface.
+    # do 'service dnsmasq restart' and 'service hostapd restart'.
+    # This may not work, so the ifconfig may show an address but the
+    # antenna isn't broadcasting.
+    backup( '/etc/network/', 'interfaces' )
+    os.system( 'cp ' + interfaces_ap_file + ' /etc/network/interfaces' )
+    time.sleep(10)
+    os.system( 'ifdown wlan0; ifup wlan0' )
+    os.system( 'service dnsmasq restart' )
+    os.system( 'service hostapd restart' ) 
+
+#
+# Program starts here.
+#
+
+# pdb.set_trace()
+do_main()
+
